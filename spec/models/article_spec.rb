77d6ea93e353da3f805f8ae6efcef998d2f47c14
Octavio Cornejo <ocornejo@arkusnexus.com>
2021-04-20 12:02:31 -0500

    Pruebas módulo Articulos

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe '#validations' do
+    let(:article) { build(:article) }
+
+    it "tests article object" do
+      expect(article.title).to eq("Sample article")
+    end
+
+    it "test that factory is valid" do
+      expect(article).to be_valid #article.valid? == true
+    end
+
+    it "has an invalid title" do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it "has an invalid content" do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it "has an invalid content" do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+    it "validates the uniqueness of slug" do
+      article1 = create(:article)
+      expect(article1).to be_valid
+      article2 = build(:article, slug: article1.slug)
+      expect(article2).not_to be_valid
+      expect(article2.errors[:slug]).to include("has already been taken")
+    end
+
+  end
+  
+end
