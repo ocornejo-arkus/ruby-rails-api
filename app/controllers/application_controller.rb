8ffe5b2e2b1b147602944f3d6d8547fc7c8b826f
Octavio Cornejo <ocornejo@arkusnexus.com>
2021-04-27 17:55:04 -0500

    Login User Endpoint - Unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::API
       'JsonapiErrorsHandler::Errors::NotFound'
   )
   rescue_from ::StandardError, with: lambda { |e| handle_error(e) }
+  rescue_from UserAuthenticator::AuthenticationError, with: :authentication_error
+
+  private
+
+  def authentication_error
+    error = {
+      "status" => "401",
+      "source" => { "pointer" => "/code" },
+      "title" => "Authentication code is invalid",
+      "detail" => "You must provide valid code in order to exhange it for token.",
+    }
+    render json: {"errors": error }, status: 401
+  end
 end
