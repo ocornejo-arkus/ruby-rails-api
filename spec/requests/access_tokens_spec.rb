5e41efe20974324551a2712f8fe8dcf06040d9fc
Octavio Cornejo <ocornejo@arkusnexus.com>
2021-04-27 18:04:17 -0500

    Shared examples

--- spec/requests/access_tokens_spec.rb
+++ spec/requests/access_tokens_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController do
   describe "#create" do
-    context 'when invalid request' do
+
+    shared_examples_for 'unauthorized_requests' do
       let(:error) do
         {
           "status": "401",
@@ -13,14 +14,24 @@ RSpec.describe AccessTokensController do
       end
 
       it 'should return 401 status code' do
-        post '/login'
+        subject
         expect(response).to have_http_status(401)
       end
 
       it 'should return proper error body' do
-        post '/login'
+        subject
         expect(json[:errors]).to eq(error)
       end
+    end
+
+    context 'when mo code provided' do
+      subject { post '/login' }
+      it_behaves_like 'unauthorized_requests'
+    end
+
+    context 'when invalid code provided' do
+      subject { post '/login', params: { code: 'invalid_code' } }
+      it_behaves_like 'unauthorized_requests'
     end
 
     context 'when success request' do
